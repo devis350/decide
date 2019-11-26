9c0a361347c6a46e092d561a9ba237f2596d889d
Turzinski, Erik <Erik.Turzinski@adesso.de>
2019-11-26 19:56:16 +0100

    added msg to Error

--- src/error.rs
+++ src/error.rs
@@ -1,30 +1,48 @@
 #[derive(Debug)]
-pub enum Error {
+pub struct Error {
+    pub error_type: ErrorType,
+    pub msg: String,
+}
+
+#[derive(Debug)]
+pub enum ErrorType {
     IO,
     Parse,
     None,
 }
 
 impl From<std::io::Error> for Error {
-    fn from(_: std::io::Error) -> Self {
-        Error::IO
+    fn from(e: std::io::Error) -> Self {
+        Self {
+            error_type: ErrorType::IO,
+            msg: e.to_string(),
+        }
     }
 }
 
 impl From<toml::de::Error> for Error {
-    fn from(_: toml::de::Error) -> Self {
-        Error::Parse
+    fn from(e: toml::de::Error) -> Self {
+        Self {
+            error_type: ErrorType::Parse,
+            msg: e.to_string(),
+        }
     }
 }
 
 impl From<std::option::NoneError> for Error {
     fn from(_: std::option::NoneError) -> Self {
-        Error::None
+        Self {
+            error_type: ErrorType::None,
+            msg: "None".to_string(),
+        }
     }
 }
 
 impl From<toml::ser::Error> for Error {
-    fn from(_: toml::ser::Error) -> Self {
-        Error::Parse
+    fn from(e: toml::ser::Error) -> Self {
+        Self {
+            error_type: ErrorType::Parse,
+            msg: e.to_string(),
+        }
     }
 }
